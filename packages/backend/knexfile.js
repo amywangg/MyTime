a9188f4ebd01a230caebfb3ab1c0ae097f011310
Amy Wang <amy_wangg@hotmail.com>
2021-11-08 14:39:14 -0500

    adds docker-compose file for postgres & redis

--- packages/backend/knexfile.js
+++ packages/backend/knexfile.js
@@ -1,6 +1,6 @@
 const path = require("path");
 require("dotenv").config({ path: path.resolve(__dirname, "./.env") });
-console.log(process.env);
+
 module.exports = {
   development: {
     client: "pg",
