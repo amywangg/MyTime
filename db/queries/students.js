6e45c40d0c863583ea7e4b69befad4a9399111d7
vivvv.li <vivian.li982@gmail.com>
2022-02-03 20:51:03 -0500

    student routes updated

--- db/queries/students.js
+++ db/queries/students.js
@@ -48,4 +48,52 @@ module.exports = {
       return err;
     }
   },
+
+  // feel like this can probably be all grouped under getStudent, up to you
+  // this returns all the info for the student progress bar and student profile (so it include sthe info from getStudent as well)
+  async studentStats(student_id) {
+    try {
+      let studentStats = await knex("students")
+        .select(["first_name", "last_name", "email","middle_name","school","hours_approved","grade","skills"])
+        .where("student_id", student_id);
+      let student = studentStats[0];
+      return student;
+    } catch (err) {
+      return err;
+    }
+  },
+
+  // this returns all the info for student-job interactions
+  // missing a column here for getting hte hours from the job; consider adding a column in the jobs table
+  async studentJobs(student_id) {
+    try {
+      let studentJobs = await knex.select("orgs.name", "postings.title","orgs.website","student_job.status","jobs.start_time")
+      .from("student_job")
+      .leftJoin("postings","postings.id","student_job.posting_id")
+      .leftJoin("jobs","jobs.posting_id","postings.id")
+      .leftJoin("orgs","orgs.id","postings.org_id")
+      .where("student_job.student_id",student_id)
+      ;
+      let student_job = studentJobs[0];
+      return student_job;
+    } catch (err) {
+      return err;
+    }
+  },
+
+  // if job length (num hours) gets added into jobs table, update the query to include that value
+  async getPosting(posting_id) {
+    try {
+      let getPosting = await knex.select("postings.title","orgs.name","orgs.website","postings.id","postings.description","student_job.status")
+      .from("postings")
+      .leftJoin("jobs","postings.id","jobs.posting_id")
+      .leftJoin("orgs","orgs.id","postings.org_id")
+      .leftJoin("student_job","student_job.posting_id","postings.id")
+      ;
+      let posting = getPosting[0];
+      return posting;
+    } catch (err) {
+      return err;
+    }
+  },
 };
