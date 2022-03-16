6ac554d9603b7b25d745816471fbbf1c3b8f048b
thushv <64873551+thushv@users.noreply.github.com>
2022-03-16 16:31:14 -0400

    Update 3-org_school.js

--- db/seeds/3-org_school.js
+++ db/seeds/3-org_school.js
@@ -7,26 +7,38 @@ exports.seed = async function (knex) {
     .then(function () {
       // Inserts seed entries
       return knex("org_school").insert([
-        {
-          org_id: 1,
-          school_id: 1,
-          status: "verified",
-        },
-        {
-          org_id: 2,
-          school_id: 1,
-          status: "",
-        },
-        { org_id: 3, school_id: 1, status: "rejected" },
+        { org_id: 1, school_id: 1, status: "verified" },
+        { org_id: 2, school_id: 1, status: "verified" },
+        { org_id: 3, school_id: 1, status: "verified" },
+        { org_id: 4, school_id: 1, status: "verified" },
+        { org_id: 5, school_id: 1, status: "verified" },
+
+        { org_id: 1, school_id: 2, status: "verified" },
+        { org_id: 2, school_id: 2, status: "verified" },
+        { org_id: 3, school_id: 2, status: "verified" },
         { org_id: 4, school_id: 2, status: "" },
-        { org_id: 5, school_id: 2, status: "" },
+        { org_id: 5, school_id: 2, status: "verified" },
+
+        { org_id: 1, school_id: 3, status: "verified" },
+        { org_id: 2, school_id: 3, status: "" },
+        { org_id: 3, school_id: 3, status: "verified" },
         { org_id: 4, school_id: 3, status: "" },
-        { org_id: 5, school_id: 3, status: "" },
+        { org_id: 5, school_id: 3, status: "verified" },
+
+        { org_id: 1, school_id: 4, status: "" },
+        { org_id: 2, school_id: 4, status: "verified" },
+        { org_id: 3, school_id: 4, status: "" },
         { org_id: 4, school_id: 4, status: "" },
         { org_id: 5, school_id: 4, status: "" },
-        { org_id: 1, school_id: 5, status: "" },
-        { org_id: 2, school_id: 5, status: "rejected" },
-        { org_id: 3, school_id: 5, status: "verified" },
+
+        { org_id: 1, school_id: 5, status: "rejected" },
+        { org_id: 2, school_id: 5, status: "" },
+        { org_id: 3, school_id: 5, status: "" },
+        { org_id: 4, school_id: 5, status: "" },
+        { org_id: 5, school_id: 5, status: "rejected" },
+
       ]);
     });
 };
+
+//org:
