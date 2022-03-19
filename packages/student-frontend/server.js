a5c9fe18bc06f2a14bc48c78eb174555a2d173cf
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 17:20:47 -0400

    update api url

--- packages/student-frontend/server.js
+++ packages/student-frontend/server.js
@@ -1,28 +1,8 @@
-// const path = require("path");
-// const express = require("express");
-// const cors = require("cors");
-
-// const app = express();
-// app.use(cors());
-// // heroku will provide a port or local
-// const port = process.env.PORT || 3002;
-
-// const publicPath = path.join(__dirname, "..", "client/build");
-
-// app.use(express.static(publicPath));
-// // app.use("/static", express.static(path.join(__dirname, "client/build")));
-
-// app.get("*", (req, res) => {
-//   const index = path.join(__dirname, "build", "index.html");
-//   res.sendFile(index);
-// });
-
-// app.listen(port, () => {
-//   console.log(`Server is up on port ${port}!`);
-// });
-
 const path = require("path");
 const express = require("express");
+const cors = require("cors");
+
+app.use(cors());
 const app = express();
 const publicPath = path.join(__dirname, "build");
 const port = process.env.PORT || 3000;
