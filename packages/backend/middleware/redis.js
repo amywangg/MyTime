edbbf4ff02c621a95d929a14f6dd277d0cbff013
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 13:30:08 -0400

    update frontend packages

--- packages/backend/middleware/redis.js
+++ packages/backend/middleware/redis.js
@@ -1,12 +1,20 @@
 const redis = require("redis");
+var url = require("url");
 // 1 configure our redis
 let redis_client;
 (async () => {
   if (process.env.REDISTOGO_URL) {
-    console.log("IAMHERE");
-    redis_client = redis.createClient(process.env.REDISTOGO_URL);
-    redis_client.on("error", (err) => console.log("Redis Client Error", err));
+    console.log("iamhere");
+    console.log(process.env.REDISTOGO_URL.split("@")[1].split(":")[0]);
+
+    var redisURL = url.parse(process.env.REDISTOGO_URL);
+    console.log(redisURL);
+    redis_client = redis.createClient({
+      url: process.env.REDISTOGO_URL,
+    });
+
     await redis_client.connect();
+    redis_client.auth("", redisURL.auth.split(":")[1]);
     await redis_client.set("key", "Successfully Connected to Redis ✨");
     console.log(await redis_client.get("key"));
   } else {
