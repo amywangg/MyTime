f22c70147179c2303959e90b4c7db96e108f56c9
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 19:25:05 -0400

    update student axios

--- packages/student-frontend/server.js
+++ packages/student-frontend/server.js
@@ -3,7 +3,7 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors({ credentials: true }));
 const publicPath = path.join(__dirname, "client/build");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
