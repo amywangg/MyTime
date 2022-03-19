3f17a02216dfe3492e3a3cb325368548fa8cc9ae
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 12:24:23 -0400

    update redis for heroku

--- packages/backend/middleware/redis.js
+++ packages/backend/middleware/redis.js
@@ -1,17 +1,20 @@
 const redis = require("redis");
-// 1 configure our redis
+var url = require("url");
+
 let redis_client = null;
+
 (async () => {
-  redis_client = redis.createClient(
-    process.env.NODE_ENV === "production" && process.env.REDISTOGO_URL
-  );
-
-  redis_client.on("error", (err) => console.log("Redis Client Error", err));
-
-  await redis_client.connect();
-
-  await redis_client.set("key", "Successfully Connected to Redis ✨");
-  console.log(await redis_client.get("key"));
+  if (process.env.NODE_ENV === "production") {
+    var rtg = require("url").parse(process.env.REDISTOGO_URL);
+    var redis = require("redis").createClient(rtg.port, rtg.hostname);
+    redis.auth(rtg.auth.split(":")[1]);
+  } else {
+    redis_client = redis.createClient();
+    redis_client.on("error", (err) => console.log("Redis Client Error", err));
+    await redis_client.connect();
+    await redis_client.set("key", "Successfully Connected to Redis ✨");
+    console.log(await redis_client.get("key"));
+  }
 })();
 
 module.exports = redis_client;
