deb2d8efc5f3e4947bc784fb623f1f8d44383aed
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 16:38:14 -0400

    webpack for heroku

---
+++ packages/student-frontend/server.js
@@ -0,0 +1,23 @@
+const path = require("path");
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+// heroku will provide a port or local
+const port = process.env.PORT || 3002;
+
+const publicPath = process.env.PORT
+  ? path.join(__dirname, "..", "build")
+  : path.join(__dirname, "..", "public");
+
+app.use(express.static(publicPath));
+app.use("/static", express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(publicPath, "index.html"));
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}!`);
+});
