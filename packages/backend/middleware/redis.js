9784729c58aa94d9f1eb5f663aabb8daf5666343
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 12:40:55 -0400

    redis update 4

--- packages/backend/middleware/redis.js
+++ packages/backend/middleware/redis.js
@@ -1,10 +1,9 @@
 const redis = require("redis");
 // 1 configure our redis
-let redis_client = null;
+let redis_client;
 (async () => {
   if (process.env.REDISTOGO_URL) {
-    var rtg = require("url").parse(process.env.REDISTOGO_URL);
-    var redis_client = redis.createClient(rtg.port, rtg.hostname);
+    redis_client = redis.createClient(process.env.REDISTOGO_URL);
     redis_client.on("error", (err) => console.log("Redis Client Error", err));
     await redis_client.connect();
     redis_client.auth(rtg.auth.split(":")[1]);
