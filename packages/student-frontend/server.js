1ab30315330d8a28bc30efb046b94fde7d8cd3b3
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 17:23:12 -0400

    update api url

--- packages/student-frontend/server.js
+++ packages/student-frontend/server.js
@@ -2,8 +2,8 @@ const path = require("path");
 const express = require("express");
 const cors = require("cors");
 
-app.use(cors());
 const app = express();
+app.use(cors());
 const publicPath = path.join(__dirname, "build");
 const port = process.env.PORT || 3000;
 app.use(express.static(publicPath));
