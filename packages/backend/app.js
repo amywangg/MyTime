20ed3ef4ea334b5a0d8f1b475e3977ec161ce488
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 19:31:14 -0400

    update backend cors

--- packages/backend/app.js
+++ packages/backend/app.js
@@ -7,23 +7,8 @@ const cookieParser = require("cookie-parser");
 
 const app = express();
 const cors = require("cors");
-const corsOptions = {
-  origin: (origin, callback) => {
-    callback(null, true);
-  },
-  methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-  allowedHeaders: [
-    "Access-Control-Allow-Origin",
-    "Origin",
-    "X-Requested-With",
-    "Content-Type",
-    "Accept",
-    "Authorization",
-  ],
-  credentials: true,
-};
 
-app.options("*", cors(corsOptions));
+app.options("*", cors());
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
