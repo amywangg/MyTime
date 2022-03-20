b78db6f10a53570319b7c88a6d06bfd15350af89
Amy Wang <amy_wangg@hotmail.com>
2022-03-19 21:14:10 -0400

    update org prod

--- packages/org-frontend/src/api/index.jsx
+++ packages/org-frontend/src/api/index.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 import TokenService from "../services/TokenService";
 
 const api = axios.create({
-  baseURL: "http://localhost:3000/orgs/",
+  baseURL: "https://mytime-backend-1.herokuapp.com/orgs/",
   timeout: 5000,
   headers: {
     "Access-Control-Allow-Origin": "*",
