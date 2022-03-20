91d35a9da8f0296c40e68605a22567b42c94a2fd
Amy Wang <amy_wangg@hotmail.com>
2022-03-20 15:25:03 -0400

    try with ioredis

--- packages/backend/middleware/redis.js
+++ packages/backend/middleware/redis.js
@@ -1,21 +1,43 @@
-const redis = require("redis");
+// const redis = require("redis");
 
-let redis_client;
-(async () => {
-  if (process.env.REDIS_TLS_URL) {
-    redis_client = redis.createClient({
-      url: process.env.REDIS_TLS_URL,
-    });
-    await redis_client.connect();
-    await redis_client.set("key", "Successfully Connected to Redis ✨");
-    console.log(await redis_client.get("key"));
-  } else {
-    redis_client = redis.createClient();
-    redis_client.on("error", (err) => console.log("Redis Client Error", err));
-    await redis_client.connect();
-    await redis_client.set("key", "Successfully Connected to Redis ✨");
-    console.log(await redis_client.get("key"));
-  }
-})();
+// let redis_client;
+// (async () => {
+//   if (process.env.REDIS_TLS_URL) {
+//     redis_client = redis.createClient({
+//       url: process.env.REDIS_TLS_URL,
+//     });
+//     await redis_client.connect();
+//     await redis_client.set("key", "Successfully Connected to Redis ✨");
+//     console.log(await redis_client.get("key"));
+//   } else {
+//     redis_client = redis.createClient();
+//     redis_client.on("error", (err) => console.log("Redis Client Error", err));
+//     await redis_client.connect();
+//     await redis_client.set("key", "Successfully Connected to Redis ✨");
+//     console.log(await redis_client.get("key"));
+//   }
+// })();
+
+// module.exports = redis_client;
+
+const redis = require("ioredis");
+
+let redis_client = new Redis(process.env.REDIS_URL);
+// (async () => {
+//   if (process.env.REDIS_TLS_URL) {
+//     redis_client = redis.createClient({
+//       url: process.env.REDIS_TLS_URL,
+//     });
+//     await redis_client.connect();
+//     await redis_client.set("key", "Successfully Connected to Redis ✨");
+//     console.log(await redis_client.get("key"));
+//   } else {
+//     redis_client = redis.createClient();
+//     redis_client.on("error", (err) => console.log("Redis Client Error", err));
+//     await redis_client.connect();
+//     await redis_client.set("key", "Successfully Connected to Redis ✨");
+//     console.log(await redis_client.get("key"));
+//   }
+// })();
 
 module.exports = redis_client;
